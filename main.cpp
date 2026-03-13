45e8c43db7711f5fb1ef65b527e9c870adaab2b3
DanielGonzalezGarrote_ExamenGit <158164494+daaaaniel242@users.noreply.github.com>
2026-03-13 10:22:34 +0100

    Subir main

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Proyecto 2" << std::endl;
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by danie on 13/03/2026.
+//
+
+#ifndef ACADEMIA_MAIN_H
+#define ACADEMIA_MAIN_H
+
+
+class main
+{
+};
+
+
+#endif //ACADEMIA_MAIN_H
